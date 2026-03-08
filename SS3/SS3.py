3a510a0e37d33a46f1e1268489f6e1a9d1d5e881
hieu1966hn <hieu1966hn@gmail.com>
2026-03-08 10:57:46 +0700

    final push SS3

--- SS3/SS3.py
+++ SS3/SS3.py
@@ -14,6 +14,15 @@ class MovieItem:
             if value: 
                 setattr(self, atttribute, value)
         
-        
+## Tạo một vài bộ phim 
+movie1 = MovieItem(1, "Avengers", "2022", None, 5, None)
+movie2 = MovieItem(1, "Pacificrim", "2013", None, 5, None)
+movie3 = MovieItem(1, "Interstella", "2013", None, 5, None)
     
-    
+## Khởi tạo kiểu dữ liệu danh sách phim: LIST
+movies = [movie1, movie2, movie3]
+
+# thêm phim mới: movies.append(movie4)
+# cập nhật thông tin phim: movies[1].update(new_data)
+# xóa một phần tử ở vị trí nào đó:  movies.pop(x) # vị trí thứ x
+# Tìm kiếm theo tên: ... về nhà làm nốt. 
---
+++ SS3/SS3_lythuyet.md
@@ -0,0 +1,3 @@
+Danh sách: là một tập hợp các phần tử có thứ tự, và chúng ta có thể truy cập vào từng phần tử của danh sách. 
+
+Danh sách có thể chứa nhiều giá trị, và các giá trị này có thể là bất kỳ kiểu dữ liệu nào .... Và nó có thể chứa một danh sách khác. 
